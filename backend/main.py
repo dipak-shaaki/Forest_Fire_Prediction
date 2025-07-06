cb5d9c8fb21ad78f962fc32c2da801163aef5c18
Dipak Shaaki <shanki.dipak@gmail.com>
2025-07-06 21:48:14 +0545

    Upto map+Prediction

--- backend/main.py
+++ backend/main.py
@@ -4,62 +4,27 @@ from fastapi.responses import JSONResponse
 import joblib
 import pandas as pd
 import math
-import csv
-import io
-import requests
 from fastapi.middleware.cors import CORSMiddleware
 
-from utils.enrich import enrich_point
+app = FastAPI()
 
-app = FastAPI(title="Nepal Forest Fire Risk API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# CORS policy
-app.add_middleware( CORSMiddleware, allow_origins=["*"], allow_credentials=True, 
-allow_methods=["*"], 
-allow_headers=["*"], )
-
-# Load model
+# Load the model
 try:
     model = joblib.load("./model/catboost_final_model.pkl")
 except Exception as e:
     model = None
-    print(f"[ERROR] Failed to load model: {e}")
+    print(f"[ERROR] Could not load model: {e}")
 
 features = ['latitude', 'longitude', 'temperature', 'humidity',
             'wind_speed', 'precipitation', 'elevation', 'vpd']
-
-@app.get("/")
-def root():
-    return {"message": "Nepal Forest Fire Risk API is live and ready!"}
-
-@app.get("/health")
-def health():
-    return {"status": "API is running!"}
-
-@app.get("/api/fires")
-def get_fires():
-    FIRMS_TOKEN = "eyJ0eXAiOiJKV1QiLCJvcmlnaW4iOiJFYXJ0aGRhdGEgTG9naW4iLCJzaWciOiJlZGxqd3RwdWJrZXlfb3BzIiwiYWxnIjoiUlMyNTYifQ.eyJ0eXBlIjoiVXNlciIsInVpZCI6ImRpcGFrMjIiLCJleHAiOjE3NTY2NTc3ODAsImlhdCI6MTc1MTQ3Mzc4MCwiaXNzIjoiaHR0cHM6Ly91cnMuZWFydGhkYXRhLm5hc2EuZ292IiwiaWRlbnRpdHlfcHJvdmlkZXIiOiJlZGxfb3BzIiwiYWNyIjoiZWRsIiwiYXNzdXJhbmNlX2xldmVsIjozfQ.N0aH9ghBlyBlboVNlMDju45MTC8kjD860Gh80Sipp4BOEYLPIZ_8VvpanorY53GD77jSNGCcRKeD0Fr9C30HNxfqw_-mcwc6dS7Hbb3JUUcfLi-4InXrHLRpGWjsxW_33KMeCKjFveEjIMn3FO_7HCmYOdLvNxPHP5XVIXj6iEW9ntFJJcYz1RCtXM4LosKl_zXK10VfFCa68ZY5IapOhaIsSKmhcbqTfWNJKw5wfIXQlztB4XfJa4biVrBgvRZwJVBPI3zMrjmHIrgOdO8DJIGhAWHzSqVpGkzXVg04jdZLPN4x2KIzBcbBV9ZpA9WPy-zLKxegFVeU-QP7YY42ww"
-   
-    try:
-        url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_Nepal_7d.csv"
-        
-        headers = {
-        "Authorization": f"Bearer {FIRMS_TOKEN}"
-    }
-        res = requests.get(url, headers=headers, timeout=10)
-
-        if res.status_code != 200:
-            return JSONResponse(status_code=500, content={"error": f"Failed to fetch FIRMS data: Status {res.status_code}"})
-
-        decoded = res.content.decode('utf-8')
-        csv_reader = csv.DictReader(io.StringIO(decoded))
-        data = [row for row in csv_reader]
-
-        return JSONResponse(content=data)
-
-    except Exception as e:
-        print(f"[ERROR] in get_fires: {e}")
-        return JSONResponse(status_code=500, content={"error": str(e)})
 
 class ManualInput(BaseModel):
     latitude: float
